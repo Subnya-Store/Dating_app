838f4d8ac054269cb6253d461040b447c32a370e
waleed naseem1 <waleed.naseem1@gmail.com>
2023-08-23 01:54:47 +0500

    Waleed

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import SignIn from '@/Components/SignIn'
 
-export default function signIn() {
+export default function signin() {
   return (
     <div>
         <SignIn/>
