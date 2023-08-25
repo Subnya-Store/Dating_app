fdbfda1f46c01cb5a4e71a74ef88756662b720d5
Farzana-khan99 <id.farzankhan@gmail.com>
2023-08-25 09:01:37 +0500

    farzana khan

---
+++ src/Components/Dashboard_section/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import LeftHeader from './LeftHeader'
+export default function index() {
+  return (
+    <div>
+        <LeftHeader/>
+        <RightHeader/>
+    </div>
+  )
+}
