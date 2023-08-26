9e16d812d02ce72e1704e38e29f8307901ca4931
Farzana-khan99 <id.farzankhan@gmail.com>
2023-08-26 21:06:20 +0500

    farzana-khan

--- src/Components/Dashboard_section/Page_one/Matches/Activity_folder/index.js
+++ src/Components/Dashboard_section/Page_one/Matches/Activity_folder/index.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {BsThreeDots} from 'react-icons/bs'
 
 export default function index() {
   return (
-    <div>index</div>
+    <div className='activity_box mt-10 w-[50%]  rounded-2xl bg-white py-6' >
+    <div className='container mx-auto px-4'>
+    <table border={1}>
+      <tr>
+        <th><h4 className='text-[#050062] text-2xl  font-bold'>Activity</h4></th>
+        <th colSpan={6} className='text-[#050062] text-2xl  font-bold'><BsThreeDots/></th>
+      </tr>
+      <tr>
+      </tr>
+    </table>
+    </div>
+    </div>
   )
 }
--- src/Components/Dashboard_section/Page_one/index.js
+++ src/Components/Dashboard_section/Page_one/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import Matches from './Matches'
-import Activity_folder from '@/Components/Dashboard_section/Page_one/Matches/Activity_folder'
-
 
 export default function index() {
   return (
     <div className="bg-[url('/Images/Dashboard_pg1.png')]  bg-center  bg-no-repeat h-screen ">
        <Matches/>
-       <Activity_folder/>
     </div>
   )
 }
