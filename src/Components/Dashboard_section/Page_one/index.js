aa9f01225471a3728f1c8e2572eaf8d299a30865
Aliya <aliya@gmail.com>
2023-09-08 01:05:47 +0500

    ALIYA

--- src/Components/Dashboard_section/Page_one/index.js
+++ src/Components/Dashboard_section/Page_one/index.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from 'react'
 import Matches from "./Matches";
-import Setting from "./Setting";
 
-export default function index() {
+// import Inbox from '@/Components/Dashboard_section/Page_one/inbox'
+
+export default function index({stateHeader,setStateHeader}) {
+//background laga du bas
   return (
     <div className=" relative bg-[url('/Images/Dashboard_pg1.png')]     w-[100%] h-screen  bg-center  bg-cover  bg-no-repeat   ">
       <div className="  absolute  before:content-[]   bg-[#0500629e]  overflow-y-scroll  bottom-0 top-0 left-0 w-[100%]  h-[100%] ">
         <Matches />
+        {/* <Inbox/> */}
     
+        {/* {
+        stateHeader == "Matches" && <Matches setStateHeader={setStateHeader} /> ||
+        stateHeader == "Inbox" && <Inbox setStateHeader={setStateHeader} /> 
+        
+      } */}
+
       </div>
     </div>
 
-    // <div className=" bg-[#fcf8f8]   py-9 overflow-y-scroll  bottom-0 top-0 left-0 w-[100%]  h-[100%] ">
-    //   <Setting />
-    // </div>
+   
   );
 }
