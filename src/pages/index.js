f079b41d7e07122591c06bc3c6446198a9c5c52a
waleed naseem1 <waleed.naseem1@gmail.com>
2023-09-11 00:05:28 +0500

    waleed

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ export default function index() {
               </div>
             </div>
           </div>
-          <div className='-mt-[500px]'>
+          <div className='-mt-[50px]'>
           <SectionAbout  />
           </div>
           <SectionSteps />
