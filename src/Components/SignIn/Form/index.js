d3c5b583572059c2e439a12ea27dda5cf5e53d0c
waleed <waleed.naseem1@gmail.com>
2023-10-03 01:54:11 +0500

    waleed

--- src/Components/SignIn/Form/index.js
+++ src/Components/SignIn/Form/index.js
@@ -14,10 +14,9 @@ export default function index() {
   }
   const Singin_logo = "/Images/SignIn_logo.png";
   return (
-
-    <div className=" md:w-1/3  bg-white p-8  my-12  mx-16 rounded-2xl shadow-lg -z-10 opacity-90">
+    <div className=" md:w-[450px]  bg-white p-8  flex flex-col  mx-16 rounded-2xl shadow-lg-z-10 opacity-90">
       <div className=" flex">
-        <ul className="flex justify-between w-72">
+        <ul className="flex justify-between w-full">
           <li>
             <img className="flex" src={Singin_logo} />
           </li>
@@ -30,11 +29,11 @@ export default function index() {
           </li>
         </ul>
       </div>
-      <div className=" text-4xl font-bold py-4 capitalize"> Sign In</div>
+      <div className=" text-4xl font-bold py-8 capitalize"> Sign In</div>
       <div className="inline-flex gap-5 pt-6 pb-6 ">
         <button className="bg-[#7000ED] flex rounded-xl text-white py-2 pr-5">
-          <img className=" object-contain px-4" src="/Images/Google_img.png " /> Sign in
-          with google
+          <img className=" object-contain px-4" src="/Images/Google_img.png " />
+          Sign in with google
         </button>
         <button className="bg-[#7000ED] flex rounded-xl text-white  px-4 p-2">
           <img src="/Images/facbook_signin.png" className=" object-contain" />
--- src/Components/SignIn/index.js
+++ src/Components/SignIn/index.js
@@ -3,14 +3,12 @@ import Form from "./Form";
 
 export default function index() {
   return (
-    // <div className="bg-[url('/Images/SignIn_img.png')] py-4 bg-center  bg-no-repeat h-screen  ">
-       <div className=" relative bg-[url('/Images/SignIn_img.png')] md:py-4 bg-center  bg-no-repeat md:h-screen  ">
-      <div className="  absolute  before:content-[]   bg-[#0500629e]    bottom-0 top-0 left-0 w-[100%]  h-[100%] ">
-
-      <Form />
-     
+    <div className="  bg-[url('/Images/SignIn_img.png')] md:py-4 bg-center  bg-no-repeat md:h-screen  ">
+      <div className="  absolute bg-[#0500629e]  bottom-0 top-0 left-0 w-[100%]  h-[100%] ">
+        <div className=" h-full flex  items-center">
+          <Form />
+        </div>
       </div>
-      </div>
-  
+    </div>
   );
 }
