5f53c163fb80c01a471c23dabfa03b8b9dc1c739
waleed <waleed.naseem1@gmail.com>
2023-10-21 00:01:34 +0500

    waleed

--- src/Components/Dashboard_section/Page_one/Chat_Request/index.js
+++ src/Components/Dashboard_section/Page_one/Chat_Request/index.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import API from '@/API/API'
+import apiUrl from '@/API/constant'
+import React,{useState,useEffect} from 'react'
 
 export default function index() {
     const array = [
@@ -40,20 +42,30 @@ export default function index() {
         }
       
     ]
+    const [how_many_request,sethow_many_request]=useState({})
+
+    useEffect(()=>{
+        API.fetchGet('/request_find')
+        .then(x=>(
+            sethow_many_request(x.data)
+        ))
+        .catch(x=>console.log(x))
+    },[])
     return (
         <div className='  bg-white md:m-4  rounded-2xl grid grid-cols-2 gap-x-8 '>
-        
-            {array.map(e =>
-                <div >
+       
+            {how_many_request.length > 0&&how_many_request.map((e,i) =>
+                <div onClick={()=>console.log(e)} key={i} >
                     <ul className='md:flex  justify-between items-center gap-3  border-[#BAAEAE] p-2  border-b m-2 '>
                         <li>
                             <figure className='md:flex md:gap-2 sm:gap-1'>
-                                <img className=' w-14 rounded-[50%]' src={e.img} />
+                                <img className=' w-14 rounded-[50%]' src={apiUrl+'/Uploads/'+e?.user.profile?.img} />
                                 <ul className='text-[#050062]'>
-                                <li className='font-bold '>{e.text}
+                                <li className='font-bold '>
+                                    {e.user.full_name}
                                 </li>
                                 <li>
-                                   {e.text2}
+                                   teacher {e?.user.profile?.age}
                                 </li>
                             </ul>
                             </figure>
@@ -61,7 +73,7 @@ export default function index() {
                     
                         <li>
                             <button className="bg-[#e4cffc] text-[#FD2579] rounded-md items-center text-center cursor-pointer font-semibold py-2 px-4">
-                                {e.button_text}
+                                reply
                             </button>
                         </li>
 
