b8b80cc29f5d4ddd8bed8ee5bd455a61ba4e5e88
waleed <waleed.naseem1@gmail.com>
2023-11-16 01:36:05 +0500

    waleed

--- src/Components/Dashboard_section/Page_one/inbox/RightSection/index.js
+++ src/Components/Dashboard_section/Page_one/inbox/RightSection/index.js
@@ -13,7 +13,7 @@ const socket = io(apiUrl)
 export default function index({ Conversations_id, setConversation_id, setProfile, Profile, recieve_msgs, setrecieve_msg, msg, setmsg }) {
   // const socket = io.8080
   const Selector_data = useSelector(x => x)
-  
+
   const messagesContainerRef = useRef(null);
 
   const [msgArray, setmsgArray] = useState({})
@@ -67,10 +67,10 @@ export default function index({ Conversations_id, setConversation_id, setProfile
         scrollToBottom()
       })
       .catch(x => console.log(x))
-  }, [msg,recieve_msgs])
+  }, [msg, recieve_msgs])
 
- 
-  useEffect(()=>{
+
+  useEffect(() => {
     socket.on('recieve_msg', (data) => {
       setrecieve_msg(data)
       Conversations_id != null && API.fetchPost({ conversation_id: Conversations_id }, '/get_conversation')
@@ -83,7 +83,7 @@ export default function index({ Conversations_id, setConversation_id, setProfile
         .catch(x => console.log(x))
       scrollToBottom()
     })
-  },[])
+  }, [])
 
 
   useEffect(() => {
@@ -143,28 +143,26 @@ export default function index({ Conversations_id, setConversation_id, setProfile
         .then(x => socket.emit('seen', 'hi')))
   }, [msgArray, isTyping]);
   return (
-    <div className='  bg-white md:m-4 mt-4  rounded-2xl md:w-[80%] w-full py-2 px-1 h-[400px] md:h-[700px] '>
-      <div >
-        <div className='flex  justify-between items-center gap-3 p-4 m-2 '>
-          <div className='flex gap-2'>
-            <img className=' w-14 rounded-[50%]' src={apiUrl + '/Uploads/' + Profile?.profile?.img || '/Images/video.png'} />
-            <div className='text-[#050062] '>
-              <div className='font-bold  mt-4'>
-                {Profile?.full_name}
-              </div>
-            </div>
-          </div>
+    <div className='  bg-white md:m-4 mt-4  rounded-2xl md:w-[80%] w-full py-2 px-1 h-[400px] md:h-[600px] '>
 
-          <div className="flex md:gap-6 gap-2">
-            <div> <img className=' w-5 h-3 md:h-6 md:w-8' src="/Images/video.png" /></div>
-            <div> <img className=' w-5  h-3 md:h-6 md:w-6' src="/Images/phone.png" /></div>
-            <div> <img className=' w-5  h-3  md:h-6 md:w-6' src="/Images/info.png" /></div>
+      <div className='flex  justify-between items-center gap-3 px-2 '>
+        <div className='flex gap-2'>
+          <img className=' w-14 rounded-[50%]' src={apiUrl + '/Uploads/' + Profile?.profile?.img || '/Images/video.png'} />
+          <div className='text-[#050062] '>
+            <div className='font-bold  mt-4'>
+              {Profile?.full_name}
+            </div>
           </div>
         </div>
 
+        <div className="flex md:gap-6 gap-2">
+          {/* <div> <img className=' w-5 h-3 md:h-6 md:w-8' src="/Images/video.png" /></div>
+          <div> <img className=' w-5  h-3 md:h-6 md:w-6' src="/Images/phone.png" /></div> */}
+          <div> <img className=' w-5  h-3  md:h-6 md:w-6' src="/Images/info.png" /></div>
+        </div>
       </div>
       <div className='h-full'>
-        <div className='overflow-y-scroll h-[75%] ' ref={messagesContainerRef} >
+        <div className='overflow-y-scroll h-[70%] ' ref={messagesContainerRef} >
           {msgArray.length > 0 && msgArray.map((e, i) =>
             <div key={i} className="">
               <div className={`flex ${e.from == User_id.id ? 'justify-end px-4 mb-5' : 'justify-start px-4'}`} >
@@ -176,8 +174,9 @@ export default function index({ Conversations_id, setConversation_id, setProfile
           )}
           <div className='mb-10'></div>
         </div>
-        {isTyping ? <p className='h-[20px] w-full '>typing...</p> : <div className='h-[20px] w-full '></div>}
-        <div>
+        {isTyping ? <p className='h-[9%] w-full '>typing...</p> : <div className='h-[9%] w-full '></div>}
+
+        <div className='h-[20%]'>
           <div className='flex justify-between bg-[#D9D9D9] rounded-full  w-full'>
             <div className=' rounded-full justify-middle flex w-full'>
               <form className="flex w-full" onSubmit={Msg_now}>
